04f442f9e1cc8ccbe004775e5ec7a5357e2adf9f
PranitiV <63542508+PranitiV@users.noreply.github.com>
2022-03-20 15:16:13 -0400

    Update main.cpp
    Created a vector of arrays - each of size 5. Wrote function to load the vector.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world!" << std::endl;
+using namespace std;
+
+vector<array<char,5>> all_words;
+
+//function declarations
+bool LoadInitialVector();
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    if (!LoadInitialVector()){
+        return 0;
+    }
+    //call other functions here 
+    return 0;
+}
+
+bool LoadInitialVector(){
+    ifstream allWords;
+    array <char,5> eachWord;
+    //Warning: Change file path when running 
+    allWords.open("wordle_dict.txt");
+    
+    if (!allWords.is_open()){
+        cout << "Error: Unable to open file \"wordle_dict.txt\""<< endl;
+        return false;
+    }
+    
+    string word;
+    int position = 0;
+    
+    while(getline(allWords,word)){
+        for (string::iterator it = word.begin(); it!=word.end(); it++, position++){
+            eachWord[position] = *it;
+        }
+        all_words.push_back(eachWord);
+        position = 0;
+    }
+    //cout << all_words.size();
+    return true;
 }
